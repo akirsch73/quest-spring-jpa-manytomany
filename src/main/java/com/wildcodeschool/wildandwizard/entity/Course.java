26ac1e4fa325370e43a3d3f4bcbc818aa1717550
Alexander Kirsch <a.kirsch@gmx.de>
2020-01-22 10:35:40 +0100

    Spring03_JPA03

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,15 @@
 package com.wildcodeschool.wildandwizard.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.*;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Course {
@@ -13,6 +19,12 @@ public class Course {
     private Long id;
     private String name;
 
+    @ManyToMany
+    @JoinTable(name= "wizard_course",
+    		joinColumns = @JoinColumn(name = "course_id"),
+    		inverseJoinColumns = @JoinColumn(name ="wizard_id"))
+    private List<Wizard> wizards;
+    
     public Course() {
     }
 
@@ -35,4 +47,14 @@ public class Course {
     public void setName(String name) {
         this.name = name;
     }
+
+	public List<Wizard> getWizards() {
+		return wizards;
+	}
+
+	public void setWizards(List<Wizard> wizards) {
+		this.wizards = wizards;
+	}
+    
+    
 }
